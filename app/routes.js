0b2701ab3f928441132cb7f7d27eb71ea2785d85
chris-barrett-ddts <chris.barrett@defra.gov.uk>
2026-04-24 17:05:50 +0100

    added some routes

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,21 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/form-handler', function(req, res) {
+
+    let page = req.session.data.pageNumber
+    if (page == "1"){
+        req.session.data.something = "1"
+        res.redirect("page-1")
+    } else if (page == "2"){
+        req.session.data.something = "2"
+        res.redirect("page-2")
+    } else if (page == "3"){
+        req.session.data.something = "3"
+        res.redirect("page-3")
+    } else {
+        req.session.data.something = "Index"
+        res.redirect("/")
+    }
+
+    })
