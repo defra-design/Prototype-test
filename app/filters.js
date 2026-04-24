368a791b8632c4b8e57b44d17d23404f3a0c8d44
chris-barrett-ddts <chris.barrett@defra.gov.uk>
2026-04-24 16:55:30 +0100

    Added some filters

--- app/filters.js
+++ app/filters.js
@@ -7,4 +7,6 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
-
+addFilter('uppercase', function (content) {
+  return content.toUpperCase()
+})
